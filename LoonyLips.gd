fd872aeb671ff4d5b11cce0631d00e3b4f2535bf
Kjetil Haugland <haugkjet@gmail.com>
2023-06-22 12:23:07 +0200

    Initial revision

---
+++ LoonyLips.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print ("Once upon a time i saw the movie Miniuns and thought is was the greatest movie of all time")
+	
+
+
