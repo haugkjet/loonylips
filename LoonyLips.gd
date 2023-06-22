be318cb1ea4ba971336b5b44f1f7416d4be01fba
Kjetil Haugland <haugkjet@gmail.com>
2023-06-22 12:54:16 +0200

    Added label and gfx folder

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,7 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print ("Once upon a time i saw the movie Miniuns and thought is was the greatest movie of all time")
+	var prompts = ["Kjetil", "Banana", "tingly", "glorious"]
+	var story =  "Once upon a time someone called %s ate a %s and became all %s. Is was a %s day"
+	# print (story % prompts)
 	
+	$DisplayText.text = story % prompts
 
 
